1537e0edbc25ea610037eb0f22f74943b7862fc2
E F <dootydooter@gmail.com>
2024-02-14 18:44:48 -0600

    Initial commit

---
+++ src/main/kotlin/AppConstants.kt
@@ -0,0 +1,9 @@
+import androidx.compose.ui.graphics.Color
+
+object AppConstants {
+    object Theme {
+        val Primary = Color(255, 192, 70)
+        val White = Color(0xeeeeee)
+        val TextDark = Color(24, 24, 24)
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,80 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.AwtWindow
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import java.awt.FileDialog
+import java.awt.Frame
+
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+            Text(text)
+        }
+    }
+
+//    LazyColumn {
+//        for (i in 0 until 1000000) {
+//            item {
+//                Text("Hello! $i")
+//            }
+//        }
+//    }
+
+
+
+
+
+
+
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication, title = "Targe") {
+        SmallTopAppBarExample()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+@Composable
+fun SmallTopAppBarExample() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                contentColor = AppConstants.Theme.TextDark,
+                backgroundColor = AppConstants.Theme.Primary,
+                title = {
+                    Text("Targe")
+                }
+            )
+        },
+    ) { innerPadding ->
+        Text("Hey There!")
+    }
+}
